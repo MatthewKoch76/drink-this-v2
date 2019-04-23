505534c50625485742a81fd339535b9450ea8c6f
MatthewKoch76 <37314071+MatthewKoch76@users.noreply.github.com>
2019-04-23 14:36:59 -0500

    fixed form errors

--- drink-app/src/app/posts/posts.service.ts
+++ drink-app/src/app/posts/posts.service.ts
@@ -53,7 +53,7 @@ export class PostsService {
 
   updatePost(id: string, title: string, content: string) {
     const post: Post = { id: id, title: title, content: content };
-    this.http.put('http://localhost:3000/api/posts/' + id, post)
+    this.http.put(`http://localhost:3000/api/posts/${id}`, post)
     .subscribe(response => console.log(response));
   }
 
